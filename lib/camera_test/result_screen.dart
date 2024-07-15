a6355443f6fb33dccbb34cdd4da1bd425e0af67f
Rohith Sri <96564163+Sriajapro@users.noreply.github.com>
2024-07-15 09:47:37 +0530

    camera result UI changes v1

--- lib/camera_test/result_screen.dart
+++ lib/camera_test/result_screen.dart
@@ -1,8 +1,10 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:keep_fresh/flutter_flow/flutter_flow_theme.dart';
+import 'package:keep_fresh/flutter_flow/flutter_flow_widgets.dart';
 
 
 
@@ -22,10 +24,11 @@ class ResultScreen extends StatefulWidget {
 
 
 class _ResultScreenState extends State<ResultScreen> {
-  String displayedText = '';
   @override
   void initState() {
     super.initState();
+var displayJsonList = jsonDecode(widget.text);
+  print('displayJson $displayJsonList, ${displayJsonList.runtimeType}');
   }
 
 
@@ -68,7 +71,158 @@ class _ResultScreenState extends State<ResultScreen> {
               ),
             ),
           ),
-          Html(data: widget.text),
+          Container(
+            padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+          child : Table(
+            border: TableBorder.all(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            children: [
+              TableRow(
+                 decoration: BoxDecoration(
+                 borderRadius: const BorderRadius.only(topLeft: Radius.circular(10.0), topRight: Radius.circular(10.0)),
+            color: FlutterFlowTheme.of(context).secondary,
+          ),
+                children: [
+                  TableCell(
+                    verticalAlignment: TableCellVerticalAlignment.middle,
+                    child: Container( 
+                    padding: const EdgeInsets.all(15),
+                    child: Text('Name',
+                  style: FlutterFlowTheme.of(context)
+                                .bodyLarge
+                                .override(
+                                  fontFamily: 'Comfortaa',
+                                  fontWeight: FontWeight.bold,
+                                  letterSpacing: 0,
+                                  color: Color.fromARGB(255, 255, 255, 255),
+                                  useGoogleFonts: GoogleFonts.asMap()
+                                                  .containsKey('Comfortaa'),   
+                                ),
+                    ),
+                  ),
+                  ),
+                  TableCell(
+                    verticalAlignment: TableCellVerticalAlignment.middle,
+                    child: Container( 
+                    padding: const EdgeInsets.all(15),
+                    child: Text('Type',
+                  style: FlutterFlowTheme.of(context)
+                                .bodyLarge
+                                .override(
+                                  fontFamily: 'Comfortaa',
+                                  fontWeight: FontWeight.bold,
+                                  letterSpacing: 0,
+                                  color: Color.fromARGB(255, 255, 255, 255),
+                                  useGoogleFonts: GoogleFonts.asMap()
+                                                  .containsKey('Comfortaa'),   
+                                ),
+                    ),
+                  ),
+                  ),
+                 TableCell(
+                    verticalAlignment: TableCellVerticalAlignment.middle,
+                    child: Container( 
+                    padding: const EdgeInsets.all(15),
+                    child: Text('Expiry Date',
+                  style: FlutterFlowTheme.of(context)
+                                .bodyLarge
+                                .override(
+                                  fontFamily: 'Comfortaa',
+                                  fontWeight: FontWeight.bold,
+                                  letterSpacing: 0,
+                                  color: Color.fromARGB(255, 255, 255, 255),
+                                  useGoogleFonts: GoogleFonts.asMap()
+                                                  .containsKey('Comfortaa'),   
+                                ),
+                    ),
+                  ),
+                  ),
+                ],
+              ),
+              for (var i = 0; i < jsonDecode(widget.text).length; i++)
+                TableRow(
+                  children: [
+                    TableCell(
+                    verticalAlignment: TableCellVerticalAlignment.middle,
+                    child: Container( 
+                    padding: const EdgeInsets.all(10),
+                    child: Text(jsonDecode(widget.text)[i]['name'].toString(),
+                    style: FlutterFlowTheme.of(context)
+                                                      .bodyMedium
+                                                      .override(
+                                                        fontFamily: 'Comfortaa',
+                                                        letterSpacing: 0.0,
+                                                        color: const Color(0xFF101518)
+                                                      ),
+                    ),
+                  ),
+                  ),
+                  TableCell(
+                    verticalAlignment: TableCellVerticalAlignment.middle,
+                    child: Container( 
+                    padding: const EdgeInsets.all(10),
+                    child: Text(jsonDecode(widget.text)[i]['type'].toString(),
+                    style: FlutterFlowTheme.of(context)
+                                                      .bodyMedium
+                                                      .override(
+                                                        fontFamily: 'Comfortaa',
+                                                        letterSpacing: 0.0,
+                                                        color: const Color(0xFF101518)
+                                                      ),
+                    ),
+                  ),
+                  ),
+                  TableCell(
+                    verticalAlignment: TableCellVerticalAlignment.middle,
+                    child: Container( 
+                    padding: const EdgeInsets.all(10),
+                    child: Text(jsonDecode(widget.text)[i]['expiry_date'].toString(),
+                    style: FlutterFlowTheme.of(context)
+                                                      .bodyMedium
+                                                      .override(
+                                                        fontFamily: 'Comfortaa',
+                                                        letterSpacing: 0.0,
+                                                        color: const Color(0xFF101518)
+                                                      ),
+                    ),
+                  ),
+                  ),
+            ],
+          )
+          // for (var i = 0; i < jsonDecode(widget.text).length; i++)
+          //   Text(widget.productData[i].toString()),
+            ],
+          ),
+          ),
+           FFButtonWidget(
+                      onPressed: () {
+                      },
+                      text: 'Save to Pantry',
+                      options: FFButtonOptions(
+                        width: 332,
+                        height: 50,
+                        padding: EdgeInsetsDirectional.fromSTEB(24, 0, 24, 0),
+                        iconPadding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                        color: FlutterFlowTheme.of(context).secondary,
+                        textStyle:
+                            FlutterFlowTheme.of(context).displaySmall.override(
+                                  fontFamily: FlutterFlowTheme.of(context)
+                                      .displaySmallFamily,
+                                  color: Color.fromARGB(255, 235, 229, 217),
+                                  letterSpacing: 0,
+                                  useGoogleFonts: GoogleFonts.asMap()
+                                      .containsKey(FlutterFlowTheme.of(context)
+                                          .displaySmallFamily),
+                                ),
+                        elevation: 3,
+                        borderSide: BorderSide(
+                          color: Colors.transparent,
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.circular(8),
+                      )    
+                      ),
         if(widget.productData.isNotEmpty)
           Container(
             width: 300,
